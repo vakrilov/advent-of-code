c5416092a40c421f3c69eb7ce400920e78b6959e
vakrilov <alexander.vakrilov@gmail.com>
2021-12-06 09:04:55 +0200

    play

--- playground.go
+++ playground.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	arr := [7]int{0, 1, 2, 3, 4, 5, 6}
+	slice := arr[0:3]
+
+	for i := 0; i < 10; i++ {
+		slice = append(slice, i+100)
+		slice[0]++
+
+		if slice[0] == arr[0] {
+			fmt.Println("Slice and array point to the same memory")
+		} else {
+			fmt.Println("...and now they don't")
+		}
+	}
+
+	fmt.Println("DONE")
 }
