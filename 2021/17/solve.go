b56ce03bda189a9c99db5b87eb1e485e28340512
vakrilov <alexander.vakrilov@gmail.com>
2021-12-17 10:35:42 +0200

    2021 17 refactor

--- 2021/17/solve.go
+++ 2021/17/solve.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-
 	//target area: x=20..30, y=-10..-5
 	// x1, x2, y1, y2 := 20, 30, -10, -5
 
@@ -22,10 +21,11 @@ func main() {
 	minY, maxY := y1, -y1
 	minX, maxX := int(math.Floor(math.Sqrt(float64(2*x1)))), x2
 
+	fmt.Printf("Range x=[%d, %d] y=[%d, %d] \n", minX, maxX, minY, maxY)
+
 	count := 0
 	for x := minX; x <= maxX; x++ {
 		for y := minY; y <= maxY; y++ {
-
 			cx, cy := 0, 0
 			vx, vy := x, y
 			for cx <= x2 && cy >= y1 {
@@ -33,10 +33,8 @@ func main() {
 					count++
 					break
 				}
-				cx += vx
-				cy += vy
-				vy--
-				vx--
+				cx, cy = cx+vx, cy+vy
+				vx, vy = vx+1, vy+1
 				if vx < 0 {
 					vx = 0
 				}
