7c3e3513c7ad649dace30a217aa891df50c317a9
vakrilov <alexander.vakrilov@gmail.com>
2022-12-05 13:56:44 +0200

    2022 05

---
+++ 2022/05/solve2.py
@@ -0,0 +1,19 @@
+import os
+f = open(os.path.dirname(__file__) + "/input.txt", "r", encoding="utf-8")
+
+stacks = [[] for _ in range(10)]
+
+lines = [l.removesuffix("\n") for l in f.readlines()]
+split = lines.index("")
+
+for line in lines[:split]:
+    for i in range(0, len(line), 4): # each stack is 4 symbols thick
+        if line[i+1].isupper(): # skip empty stacks
+            stacks[1 + i//4].insert(0, line[i+1])
+
+for line in lines[split + 1:]:
+    [move, fr, to] = [int(x) for x in line.split(" ")[1::2]]
+    stacks[to].extend(stacks[fr][-move:])
+    del stacks[fr][-move:]
+
+print("Part 2: ", "".join([s[-1] for s in stacks if len(s[-1:]) > 0]))
