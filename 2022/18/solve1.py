d94ecbb664c21ab9c19285f4dd7d5b4e67ec7df5
vakrilov <alexander.vakrilov@gmail.com>
2022-12-18 22:21:24 +0200

    simplify

--- 2022/18/solve1.py
+++ 2022/18/solve1.py
@@ -1,53 +1,48 @@
 import os
+from itertools import product
 f = open(os.path.dirname(__file__) + "/input.txt", "r", encoding="utf-8")
 
-L = 21
-lines = [l.removesuffix("\n") for l in f.readlines()]
+MAX = 21
 cubes = []
-for line in lines:
+for line in f.readlines():
     cubes.append(tuple(int(x)+1 for x in line.split(",")))
 
 
 def dist(a, b):
-    return abs(a[0] - b[0]) + abs(a[1] - b[1]) + abs(a[2] - b[2])
+    return sum(abs(d1 - d2) for d1, d2 in zip(a, b))
 
 
-part1_res = 0
-for i in cubes:
-    sides = 6
-    for j in cubes:
-        if i != j and dist(i, j) == 1:
-            sides -= 1
-    part1_res += sides
-
-print("Part 1:", part1_res)
+print("Part 1:", len(cubes)*6 - sum(1 for i,
+      j in product(cubes, cubes) if i != j and dist(i, j) == 1))
 
 
 def in_bounds(x, y, z):
     return \
-        0 <= x and x <= L and \
-        0 <= y and y <= L and \
-        0 <= z and z <= L
+        0 <= x and x <= MAX and \
+        0 <= y and y <= MAX and \
+        0 <= z and z <= MAX
+
+
+def get_neighbors(x, y, z):
+    return (
+        (x+1, y, z),
+        (x-1, y, z),
+        (x, y+1, z),
+        (x, y-1, z),
+        (x, y, z+1),
+        (x, y, z-1)
+    )
 
 
 reached = {(0, 0, 0)}
 stack = [(0, 0, 0)]
 part2_res = 0
 while len(stack) > 0:
-    current = stack.pop()
+    neighbors = get_neighbors(*stack.pop())
 
-    other = [
-        (current[0]+1, current[1], current[2]),
-        (current[0]-1, current[1], current[2]),
-        (current[0], current[1]+1, current[2]),
-        (current[0], current[1]-1, current[2]),
-        (current[0], current[1], current[2]+1),
-        (current[0], current[1], current[2]-1),
-    ]
+    part2_res += sum((1 for x in neighbors if x in cubes))
 
-    part2_res += sum((1 for x in other if x in cubes))
-
-    for n in other:
+    for n in neighbors:
         if in_bounds(*n) and n not in reached and n not in cubes:
             reached.add(n)
             stack.append(n)
