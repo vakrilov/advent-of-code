ef702c4c0807c7c158ce261db383802b453dc16e
vakrilov <alexander.vakrilov@gmail.com>
2023-12-04 14:03:09 +0200

    template

---
+++ 2023/template/solve.py
@@ -0,0 +1,14 @@
+import os
+f = open(os.path.dirname(__file__) + "/input.txt", "r", encoding="utf-8")
+
+
+def solve(line: str, length: int):
+    return 0
+
+
+lines = [l.removesuffix("\n") for l in f.readlines()]
+res = 0
+for line in lines:
+    res += 1
+
+print(res)
