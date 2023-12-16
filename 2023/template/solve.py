ee4d28e1406cf02d5129b01213e4245ba379d3bb
vakrilov <alexander.vakrilov@gmail.com>
2023-12-16 10:17:30 +0200

    simplify template

--- 2023/template/solve.py
+++ 2023/template/solve.py
@@ -1,14 +1,5 @@
 import os
+
 f = open(os.path.dirname(__file__) + "/sample.txt", "r", encoding="utf-8")
 
-
-def solve(line: str, length: int):
-    return 0
-
-
 lines = [l.removesuffix("\n") for l in f.readlines()]
-res = 0
-for line in lines:
-    res += 1
-
-print(res)
