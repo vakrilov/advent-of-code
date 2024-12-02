13f9bec78a59b8ff885e85f5b663d42bd09bf0e1
vakrilov <alexander.vakrilov@gmail.com>
2024-12-02 08:25:38 +0200

    2024 day2 cleanup

--- 2024/2/solve.py
+++ 2024/2/solve.py
@@ -5,62 +5,40 @@ f = open(os.path.dirname(__file__) + "/input.txt", "r", encoding="utf-8")
 
 lines = [l.removesuffix("\n") for l in f.readlines()]
 
+
 # %%
-
-count = 0
-
-for line in lines:
-    nums = [int(n) for n in line.split(" ")]
-
+def is_safe(nums, can_skip=False):
     direction = nums[0] > nums[1]
+    prev = nums[0]
+    for i in range(1, len(nums)):
+        next = nums[i]
+        diff = abs(prev - next)
+        safe = diff >= 1 and diff <= 3 and (prev > next) == direction
 
-    safe = 1
-    for i in range(0, len(nums) - 1):
-        num1 = nums[i]
-        num2 = nums[i + 1]
-        diff = abs(num1 - num2)
-        if diff < 1 or diff > 3 or (num1 > num2) != direction:
-            safe = 0
-            break
-    count += safe
+        if safe:
+            prev = next
+            continue
 
-
-print(count)
-# %%
-
-
-def is_valid(nums):
-    direction = nums[0] > nums[1]
-    for i in range(0, len(nums) - 1):
-        num1 = nums[i]
-        num2 = nums[i + 1]
-        diff = abs(num1 - num2)
-        if diff < 1 or diff > 3 or (num1 > num2) != direction:
+        if can_skip:
+            can_skip = False
+        else:
             return False
+
     return True
 
 
 count = 0
+for line in lines:
+    nums = [int(n) for n in line.split(" ")]
+    count += is_safe(nums)
+print("part1:", count)
+# %%
+count = 0
 
 for line in lines:
     nums = [int(n) for n in line.split(" ")]
-
-    print("original", nums)
-    safe = is_valid(nums)
-
-    if not safe:
-        for i in range(0, len(nums)):
-            nums_fixed = nums[:i] + nums[i + 1 :]
-            print("fixed", nums_fixed)
-            safe = is_valid(nums_fixed)
-            if safe:
-                print("FOUND!", nums_fixed)
-                safe = 1
-                break
-
+    safe = is_safe(nums, can_skip=True) or is_safe(nums[1:], can_skip=False)
     count += safe
-
-
-print(count)
+print("part2:", count)
 
 # %%
