fea218bc6a52e03e686adf6780a1b53b666100dd
vakrilov <alexander.vakrilov@gmail.com>
2024-12-03 07:31:45 +0200

    2024 day3 part2

--- 2024/3/solve.py
+++ 2024/3/solve.py
@@ -3,27 +3,41 @@ import os
 import re
 
 f = open(os.path.dirname(__file__) + "/input.txt", "r", encoding="utf-8")
-
 lines = [l.removesuffix("\n") for l in f.readlines()]
+# %%
+mul = r"mul\([0-9]{1,3},[0-9]{1,3}\)"
+do = r"do\(\)"
+dont = r"don\'t\(\)"
+regex = "|".join([mul, do, dont])
+
 
 # %%
+def get_mul(group):
+    group = group.replace("mul(", "").replace(")", "")
+    nums = [int(x) for x in group.split(",")]
+    return nums[0] * nums[1]
 
 
-def solve_line(line):
-    res = re.findall(r"mul\([0-9]{1,3},[0-9]{1,3}\)", line)
-
-    result = 0
-    for r in res:
-        r = r.replace("mul(", "").replace(")", "")
-        r = r.split(",")
-        num1 = int(r[0])
-        num2 = int(r[1])
-        result += num1 * num2
-    return result
-
-
-result = sum([solve_line(line) for line in lines])
-
+# %%
+result = 0
+for line in lines:
+    for match in re.findall(mul, line):
+        result += get_mul(match)
 print("part1", result)
+
+
+# %%
+enabled = True
+result = 0
+for line in lines:
+    for match in re.findall(regex, line):
+        if match == "don't()":
+            enabled = False
+        elif match == "do()":
+            enabled = True
+        elif enabled:
+            result += get_mul(match)
+
+print("part2", result)
 
 # %%
