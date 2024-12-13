4eb78c60465fe280313168c204e15444537af4e9
vakrilov <alexander.vakrilov@gmail.com>
2024-12-13 06:53:16 +0200

    extend template

--- 2024/template/solve.py
+++ 2024/template/solve.py
@@ -5,4 +5,10 @@ f = open(os.path.dirname(__file__) + "/sample.txt", "r", encoding="utf-8")
 
 lines = [l.removesuffix("\n") for l in f.readlines()]
 
+print(len(lines))
+print(len(lines[0]))
+
+L = len(lines)
+board = [[int(c) for c in l] for l in lines]
+
 # %%
