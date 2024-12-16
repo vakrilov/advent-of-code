91b29f9d3f3a036c0ebeaff53590fafeb8e1e4f9
vakrilov <alexander.vakrilov@gmail.com>
2024-12-16 08:29:10 +0200

    2024 day16 part2

--- 2024/16/solve.py
+++ 2024/16/solve.py
@@ -6,12 +6,12 @@ f = open(os.path.dirname(__file__) + "/input.txt", "r", encoding="utf-8")
 
 lines = [l.removesuffix("\n") for l in f.readlines()]
 
-print(len(lines))
-print(len(lines[0]))
+# print(len(lines))
+# print(len(lines[0]))
 
 L = len(lines)
 board = [[c for c in l.replace("S", ".").replace("E", ".")] for l in lines]
-print(board)
+# print(board)
 start = (L - 2, 1)
 end = (1, L - 2)
 
@@ -19,52 +19,88 @@ dirs = {0: (0, 1), 1: (1, 0), 2: (0, -1), 3: (-1, 0)}
 
 # %%
 
-visited = set()
-
 
 def step(pos):
-    row, col, dir, score = pos
+    row, col, dir, score, track = pos
 
     next_pos = []
 
-    forward = (row + dirs[dir][0], col + dirs[dir][1])
+    forward_pos = (row + dirs[dir][0], col + dirs[dir][1])
 
-    if board[forward[0]][forward[1]] == ".":
-        next_pos.append((forward[0], forward[1], dir, score + 1))
+    if board[forward_pos[0]][forward_pos[1]] == ".":
+        next_pos.append(
+            (forward_pos[0], forward_pos[1], dir, score + 1, track + (forward_pos,))
+        )
 
     right_dir = (dir + 1) % 4
     right_pos = (row + dirs[right_dir][0], col + dirs[right_dir][1])
 
     if board[right_pos[0]][right_pos[1]] == ".":
-        next_pos.append((right_pos[0], right_pos[1], right_dir, score + 1001))
+        next_pos.append(
+            (right_pos[0], right_pos[1], right_dir, score + 1001, track + (right_pos,))
+        )
 
     left_dir = (dir - 1) % 4
     left_pos = (row + dirs[left_dir][0], col + dirs[left_dir][1])
     if board[left_pos[0]][left_pos[1]] == ".":
-        next_pos.append((left_pos[0], left_pos[1], left_dir, score + 1001))
+        next_pos.append(
+            (left_pos[0], left_pos[1], left_dir, score + 1001, track + (left_pos,))
+        )
 
     return next_pos
 
 
 queue = PriorityQueue()
 
-queue.put((0, (start[0], start[1], 0, 0)))
+queue.put(
+    (
+        0,
+        (
+            start[0],
+            start[1],
+            0,
+            0,
+            (start,),
+        ),
+    )
+)
 
+
+visited = {}
+found_score = 0
+found_tracks = []
 while not queue.empty():
-    _, pos = queue.get()
+    score, pos = queue.get()
 
     mark = (pos[0], pos[1], pos[2])
-    if mark in visited:
+    if mark in visited and visited[mark] < score:
         continue
-
-    visited.add(mark)
+    visited[mark] = score
 
     if pos[0] == end[0] and pos[1] == end[1]:
-        print(pos[3])
+        found_score = pos[3]
+        found_tracks.append(pos[4])
         break
 
     next_positions = step(pos)
     for next in next_positions:
         queue.put((next[3], next))
+
+print("part1:", found_score)
+
+while not queue.empty():
+    score, pos = queue.get()
+    if score == found_score:
+        if pos[0] == end[0] and pos[1] == end[1]:
+            found_tracks.append(pos[4])
+    else:
+        break
+
+track_tiles = set()
+for track in found_tracks:
+    for t in track:
+        track_tiles.add(t)
+
+print("part2:", len(track_tiles))
 
 # %%
