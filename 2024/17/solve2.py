f46d3d1f4ea9d89fb13747865df2247f7d4eb822
vakrilov <alexander.vakrilov@gmail.com>
2024-12-17 18:34:50 +0200

    2024 day17 part2

--- 2024/17/solve2.py
+++ 2024/17/solve2.py
@@ -1,14 +1,5 @@
-# %%
-
-# Register A: 38610541
-# Register B: 0
-# Register C: 0
-
-# Program: 2,4, 1,1, 7,5, 1,5, 4,3, 5,5,  0,3,  3,0
-
-
-def program(initalA):
-    A = initalA
+def program(initial_a):
+    A = initial_a
     B = 0
     C = 0
 
@@ -22,44 +13,53 @@ def program(initalA):
         output.append(B % 8)  # 5,5
         A = A // 8  # 0,3
 
-    # print(str(output).replace("[", "").replace("]", "").replace(", ", ","))
     return output
-
-
-for i in range(0, 8):
-    print("A:", i, "out:", program(i))
 
 
 # %%
 target = [2, 4, 1, 1, 7, 5, 1, 5, 4, 3, 5, 5, 0, 3, 3, 0]
-# target.reverse()
 
 A = 0
 for pos in range(16):
-    current_target = target[-pos-1:]
+    current_target = target[-pos - 1 :]
     print("Target: ", current_target)
 
-    possibles =[]
+    possibles = []
     for i in range(0, 8):
         newA = A * 8 + i
         out = program(newA)
         print("newA", newA, "Out: ", out)
         if out == current_target:
             print("Found A: ", i)
-            # A = newA
             possibles.append(newA)
     print("Possibles: ", possibles)
     A = possibles[0]
     print("A is now: ", A)
-        
+
 
 print("A: ", A)
 print(program(A))
 
 # %%
-# for i in range(20534862392785*8, 20534862392785
 
 
-# %%
-bin(4)
+def find_A(target: list, A: int):
+    if len(target) == 0:
+        return [A]
+
+    tail = target[1:]
+    tailAs = find_A(tail, A)
+    answers = []
+    for tailA in tailAs:
+        for i in range(0, 8):
+            newA = tailA * 8 + i
+            out = program(newA)
+            if out == target:
+                answers.append(newA)
+    return answers
+
+
+answers = find_A(target, 0)
+print("part 2:", answers[0])
+
 # %%
