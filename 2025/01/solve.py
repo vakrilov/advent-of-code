743abdf727453dcbe58198f2aa0b02b7b4c28d3f
vakrilov <alexander.vakrilov@gmail.com>
2025-12-02 00:44:44 +0200

    2025 day 1

--- 2025/01/solve.py
+++ 2025/01/solve.py
@@ -1,14 +1,39 @@
 # %%
 import os
 
-f = open(os.path.dirname(__file__) + "/sample.txt", "r", encoding="utf-8")
-
+f = open(os.path.dirname(__file__) + "/input.txt", "r", encoding="utf-8")
+# f = open(os.path.dirname(__file__) + "/sample.txt", "r", encoding="utf-8")
 lines = [l.removesuffix("\n") for l in f.readlines()]
 
-print(len(lines))
-print(len(lines[0]))
+# %%
+current = 50
+zeros = 0
+for ln in lines:
+    diff = (-1 if ln[0] == "L" else 1) * int(ln[1:])
+    current += diff
+    current %= 100
+    if current == 0:
+        zeros += 1
+print("part1:", zeros)
 
-L = len(lines)
-board = [[int(c) for c in l] for l in lines]
+
+# %%
+current = 50
+passedThroughZeros = 0
+for ln in lines:
+    diff = (-1 if ln[0] == "L" else 1) * int(ln[1:])
+    if current + diff >= 100:
+        passes = (current + diff) // 100
+        passedThroughZeros += passes
+
+    if current + diff <= 0:
+        passes = (-(current + diff)) // 100 + (1 if current > 0 else 0)
+        passedThroughZeros += passes
+
+    current += diff
+    current %= 100
+
+
+print("part2:", passedThroughZeros)
 
 # %%
