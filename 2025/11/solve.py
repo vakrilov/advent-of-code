47c87d2efab0c5f97092e43ffa102d2feb4d182e
vakrilov <alexander.vakrilov@gmail.com>
2025-12-11 22:32:52 +0200

    2025 day 11

--- 2025/11/solve.py
+++ 2025/11/solve.py
@@ -1,14 +1,41 @@
 # %%
 import os
+from functools import cache
 
-f = open(os.path.dirname(__file__) + "/sample.txt", "r", encoding="utf-8")
+
+# f = open(os.path.dirname(__file__) + "/sample.txt", "r", encoding="utf-8")
+# f = open(os.path.dirname(__file__) + "/sample2.txt", "r", encoding="utf-8")
+f = open(os.path.dirname(__file__) + "/input.txt", "r", encoding="utf-8")
 
 lines = [l.removesuffix("\n") for l in f.readlines()]
 
-print(len(lines))
-print(len(lines[0]))
+nodes = dict()
 
-L = len(lines)
-board = [[int(c) for c in l] for l in lines]
+for line in lines:
+    parts = line.split(": ")
+    name = parts[0]
+    nodes[name] = parts[1].split(" ")
+
+nodes["out"] = []
+
+# %%
+
+
+@cache
+def paths(start, to):
+    if start == to:
+        return 1
+    return sum(paths(n, to) for n in nodes[start])
+
+
+print("part1: ", paths("you", "out"))
+
+dac_fft = paths("svr", "dac") * paths("dac", "fft") * paths("fft", "out")
+fft_dac = paths("svr", "fft") * paths("fft", "dac") * paths("dac", "out")
+print("part2: ", dac_fft + fft_dac)
+
+
+# %%
+
 
 # %%
